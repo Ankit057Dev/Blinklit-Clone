2b58436d16c980833b093181c7f05ce9c84bc186
Ankit057Dev <ankit03457@gmail.com>
2024-11-11 01:02:10 +0530

    DB Model Created

---
+++ server/config/connectDB.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+if(process.env.MONGODB_URI){
+    throw new Error(
+        "please provide MONGODB_URI inn the new .env file "
+    )
+}
+
+async function connectDB(){
+    try{
+
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("connected DB")
+
+    }catch (error){
+        console.log("Mongodb connection error",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import cors from  'cors'
+import dotenv from 'dotenv' // to access frontend url inside ths file
+dotenv.config()
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan' // it is logger when an api is called displayed inside the termianl
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
+const app = express()
+app.use(cors({
+    credentials : true,
+    origin: process.env.FRONTEND_URL // for accessing cookies in client side
+}))
+
+//for coverrsion of response to json
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+
+    crossOriginResourcePolicy : false  // to avoid error during we use frontend and to avoid backend in other domain error
+
+}))
+// so this was the configuration part which is done
+
+// now we will add port to run server as when server runs it uses a specific port
+
+const PORT = 8080 || process.env.PORT //to avoid port busy in some cases we add env port
+
+app.get("/",(request,response)=>{
+   // to send data from server to clientt side 
+     response.json({
+        message : "Server is running" + PORT
+     })
+})
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("server is running",PORT)
+    })
+})
