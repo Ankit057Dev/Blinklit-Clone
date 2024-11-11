d05659a3a69f88c757a2f8451f88048d29dd7b65
Ankit057Dev <ankit03457@gmail.com>
2024-11-11 23:48:04 +0530

    email verifcation message template

---
+++ server/utils/verifyEmailTemplate.js
@@ -0,0 +1,8 @@
+const verifyEmailTemplate = (name,url)=>{
+    return `
+    <p>Dear</p>
+    <p>Thank You for registring BlinkitClone.</p>
+    <a href=${url} style="color:#white;background: blue;margin-top : 10px">
+        Verify Email
+    </a> `
+}
