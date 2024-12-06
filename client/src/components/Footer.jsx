7992e55544fe8e08d15f3e163da730f2582a929f
Ankit057Dev <ankit03457@gmail.com>
2024-12-06 15:25:02 +0530

    layouts design done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { CiFacebook } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
 
 const Footer = () => {
   return (
     <footer className='border-t'>
-      <div className='container mx-auto p-4'>
+      <div className='container mx-auto p-4 text-center flex-col lg:justify-between lg:flex-row gap-2'>
       <p>© All Rights Reserved 2025</p>
-
-
       </div>
        
-      
+      <div className='flex items-center gap-4 justify-center text-2xl'>
+        <a href='' className='hover:text-primary-200'>
+        <CiFacebook/>
+        </a>
+        <a href='' className='hover:text-primary-200'>
+          <FaInstagram/>
+        </a>
+        <a href='' className='hover:text-primary-200'>
+        <CiLinkedin/>
+        </a>
+      </div>
     </footer>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-  <header className='h-20 shadow-md'>
-  logo
+  <header className='h-20 shadow-md sticky top-0'>
+  {/**logo*/}
+  
+
+  {/**search */}
+
+
+  {/**login to my cart */}
   </header>
   )
 }
