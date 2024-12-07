559dfccf146fe9a24560e9154b38f0d9c11ab6ef
Ankit057Dev <ankit03457@gmail.com>
2024-12-07 21:58:49 +0530

    components buildup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'
+import Search from './Search'
 const Header = () => {
   return (
   <header className='h-20 shadow-md sticky top-0'>
-  {/**logo*/}
-  
+ <div className=' container mx-auto flex items-center h-full justify-between'>
+      {/**logo*/}
+      <div className='h-full'>
+              <div className='h-full flex items-center px-2 '>
+                    <img 
+                      src={logo}
+                      width={170}
+                      height={60}
+                      alt='logo'
+                      className='hidden lg:block'
+                    />
+                    <img 
+                      src={logo}
+                      width={120}
+                      height={60}
+                      alt='logo'
+                      className='lg:hidden'
+                      />
+              </div>
+      </div>
 
-  {/**search */}
+      {/**search */}
+
+      <div>
+        <Search/>
+
+      </div>
 
 
-  {/**login to my cart */}
+      {/**login to my cart */}
+
+      <div>
+          Login and my cart
+      </div>
+
+
+ </div>
   </header>
   )
 }
